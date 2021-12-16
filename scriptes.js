3d5a9874b31251f7b191b377d1b2c01af1b0ef8a
Andreoli-Matteo <andreoli.matteo@iisgalvanimi.edu.it>
2021-12-16 13:51:55 +0000

    ninja

---
+++ scriptes.js
@@ -0,0 +1,12 @@
+var running = [
+    "png/Run__000.png",
+    "png/Run__001.png",
+    "png/Run__002.png",
+    "png/Run__003.png",
+    "png/Run__004.png",
+    "png/Run__005.png",
+    "png/Run__006.png",
+    "png/Run__007.png",
+    "png/Run__008.png",
+    "png/Run__009.png"
+];
